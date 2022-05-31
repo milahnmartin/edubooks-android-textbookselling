c8082b2128228b75478252271d8675dc6b112a23
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-05-31 18:43:26 +0200

    feat(activity):Added Main,Login,Account

--- app/src/main/java/edubooks/main/Activity/AccountActivity.java
+++ app/src/main/java/edubooks/main/Activity/AccountActivity.java
@@ -1,14 +1,16 @@
-package edubooks.main;
+package edubooks.main.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import edubooks.main.R;
+
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account);
     }
 }
---
+++ app/src/main/java/edubooks/main/controllers/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package edubooks.main.controllers;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    final private static String UserName = "doadmin";
+    final private static String Password = "AVNS_iFgMFL58Qnl6NqD";
+    final private static String HostName = "edu-books-db-do-user-10572576-0.b.db.ondigitalocean.com";
+    final private static int PortNumber = 25060;
+    final private static String DatabaseNameDevelopment = "development_edubooks";
+    final private String DatabaseNameProduction = "production_edubooks";
+
+    public static Connection getConnection() throws Exception {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://"+ HostName + ":"+ PortNumber+"/"+DatabaseNameDevelopment,UserName,Password);
+        }catch(Exception e){
+            throw new Exception(e);
+        }
+
+
+    }
+}
