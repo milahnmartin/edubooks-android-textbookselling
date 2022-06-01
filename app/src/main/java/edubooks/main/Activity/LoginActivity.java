f48d47c5ee7b268dee1304c23bf6b44f06fa2c28
Mikael Ismail <mikaelclayton@gmail.com>
2022-06-01 19:37:27 +0200

    Did the ux of the login page.

--- app/src/main/java/edubooks/main/Activity/LoginActivity.java
+++ app/src/main/java/edubooks/main/Activity/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import edubooks.main.R;
 import edubooks.main.controllers.DatabaseConnection;
@@ -14,6 +15,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView UsernameStr = findViewById(R.id.Username);
+        TextView PasswordStr = findViewById(R.id.Password);
     }
     public void handleLogin(View v){
         DatabaseConnection EduDB = new DatabaseConnection(this);
