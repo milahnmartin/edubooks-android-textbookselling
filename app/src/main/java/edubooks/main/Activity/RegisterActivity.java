4a0d796b55d85f5fbe6d212186c8c7a370c747d1
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-06-01 17:35:02 +0200

    feat(RegisterActivity): Added handleLogin Method

--- app/src/main/java/edubooks/main/Activity/RegisterActivity.java
+++ app/src/main/java/edubooks/main/Activity/RegisterActivity.java
@@ -3,7 +3,10 @@ package edubooks.main.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
 import edubooks.main.R;
+import edubooks.main.controllers.DatabaseConnection;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setTitle("EDUBOOKS - REGISTER");
+    }
+
+    public void handleLogin(View v){
+        DatabaseConnection EduDB = new DatabaseConnection(this);
+        Boolean userValid = EduDB.validateUser("userEmail","UserPassword");
     }
 }
