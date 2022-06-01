6b3fe093f58ad93492853f7fb057868f577807e8
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-06-01 16:11:10 +0200

    feat(SQLConnection):AddedSQLite3

--- app/src/main/java/edubooks/main/controllers/DatabaseConnection.java
+++ app/src/main/java/edubooks/main/controllers/DatabaseConnection.java
@@ -1,23 +1,76 @@
 package edubooks.main.controllers;
 
-import java.sql.*;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
 
-public class DatabaseConnection {
-    final private static String UserName = "doadmin";
-    final private static String Password = "AVNS_iFgMFL58Qnl6NqD";
-    final private static String HostName = "edu-books-db-do-user-10572576-0.b.db.ondigitalocean.com";
-    final private static int PortNumber = 25060;
-    final private static String DatabaseNameDevelopment = "development_edubooks";
-    final private String DatabaseNameProduction = "production_edubooks";
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-    public static Connection getConnection() throws Exception {
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://"+HostName+":"+PortNumber+"/"+DatabaseNameDevelopment,UserName,Password);
-        }catch(Exception e){
-            throw new Exception(e);
-        }
+import androidx.annotation.Nullable;
+
+public class DatabaseConnection extends SQLiteOpenHelper  {
+
+    public DatabaseConnection(Context context) {
+        super(context,"edubooksDB.db",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+//        db.execSQL("create table Account(id INTEGER PRIMARY KEY AUTOINCREMENT,FirstName TEXT,LastName TEXT,EmailAddress TEXT,Password TEXT,PhoneNumber TEXT)");
+        db.execSQL("create table TestLogin(id INTEGER PRIMARY KEY AUTOINCREMENT,FirstName TEXT,LastName TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists TestLogin");
+    }
+
+    public Boolean insertAccountDetails(String FirstName,String LastName,String EmailAddress,String Password,String PhoneNumber){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("FirstName",FirstName);
+        contentValues.put("LastName",LastName);
+        contentValues.put("EmailAddress",EmailAddress);
+        contentValues.put("Password",Password);
+        contentValues.put("PhoneNumber",PhoneNumber);
+        long result = DB.insert("Account",null,contentValues);
+
+        return result != -1;
+    }
+
+    public Boolean insertTestlogin(String FirstName,String LastName){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("FirstName",FirstName);
+        contentValues.put("LastName",LastName);
+        long result = DB.insert("TestLogin",null,contentValues);
+        return result != -1;
+    }
+    public Cursor getTestInfo(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        return DB.rawQuery("select * from TestLogin",null);
+    }
 
 
+    public Boolean updateAccountDetails(String FirstName,String LastName,String EmailAddress,String Password,String PhoneNumber,int userId){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("FirstName",FirstName);
+        contentValues.put("LastName",LastName);
+        contentValues.put("LastName",LastName);
+        contentValues.put("EmailAddress",EmailAddress);
+        contentValues.put("Password",Password);
+        contentValues.put("PhoneNumber",PhoneNumber);
+        long result = DB.update("Account",contentValues,"Id=?",new String[]{String.valueOf(userId)});
+
+        return result != -1;
+    }
+//
+    public Cursor getAccountInfo(int userID){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("select * from Account where id = ?",new String[]{String.valueOf(userID)});
+        return cursor;
     }
 }
