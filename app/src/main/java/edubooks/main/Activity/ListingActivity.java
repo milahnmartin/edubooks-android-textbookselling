b95f4cc109acb1f3f497d1e3b86657ebb4a5afd0
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-06-07 20:12:59 +0200

    feat(ListingActivity): Added Listing Activity

---
+++ app/src/main/java/edubooks/main/Activity/ListingActivity.java
@@ -0,0 +1,14 @@
+package edubooks.main.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edubooks.main.R;
+import android.os.Bundle;
+
+public class ListingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listing);
+    }
+}
