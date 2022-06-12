e969cf57402a3aac1db8f10e9921755b387f8088
Louis-Bat17 <75789024+Louis-Bat17@users.noreply.github.com>
2022-06-12 16:05:41 +0200

    Database entries done
    Added database entry from where it will then potentially redirect but all buttons for now only display success message.

--- app/src/main/java/edubooks/main/Activity/create_listing.java
+++ app/src/main/java/edubooks/main/Activity/create_listing.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,11 +15,16 @@ import android.widget.TextView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.Objects;
 
 import edubooks.main.R;
+import edubooks.main.controllers.DatabaseConnection;
+import edubooks.main.controllers.PasswordCreation;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -37,7 +43,7 @@ public class create_listing extends AppCompatActivity {
 
         TextView BookTitleTextView = findViewById(R.id.BookTitle);
         TextView AuthorNameTextView = findViewById(R.id.AuthorName);
-        TextView ISBDTextView = findViewById(R.id.ISBNnr);
+        TextView ISBNTextView = findViewById(R.id.ISBNnr);
         TextView BookCategory = findViewById(R.id.Category);
         TextView BookFaculty = findViewById(R.id.Faculty);
         TextView SellingPriceTextView = findViewById(R.id.SellingPrice);
@@ -55,7 +61,7 @@ public class create_listing extends AppCompatActivity {
                 if(
                         BookTitleTextView.getText().toString().equals("") ||
                         AuthorNameTextView.getText().toString().equals("") ||
-                        ISBDTextView.getText().toString().equals("") ||
+                        ISBNTextView.getText().toString().equals("") ||
                         BookCategory.getText().toString().equals("") ||
                         BookFaculty.getText().toString().equals("") ||
                         SellingPriceTextView.getText().toString().equals("") ||
@@ -64,14 +70,44 @@ public class create_listing extends AppCompatActivity {
                     Snackbar.make(view, "One or more fields are empty", Snackbar.LENGTH_SHORT).show();
                 }
                 else{
-                    if(!ISBNValid(ISBDTextView.getText().toString())){
+                    if(!ISBNValid(ISBNTextView.getText().toString())){
                         Snackbar.make(view, "Invalid ISBN Number", Snackbar.LENGTH_SHORT).show();
                     }
                     else if(!priceValid(SellingPriceTextView.getText().toString())){
                         Snackbar.make(view, "Invalid Price, ensure 1/2 decimals.", Snackbar.LENGTH_SHORT).show();
                     }
                     else{
-                        Snackbar.make(view, "Book Listed!", Snackbar.LENGTH_SHORT).show();
+                        //____________________________
+                        DatabaseConnection DatabaseConnectionObj = new DatabaseConnection(create_listing.this);
+    //insertNewBook(String Title*, String Author*, String Category*, String faculty*, String Quality* ,int IsbnNumber*, boolean isAvailible*,float bookPrice*,int accountId*)
+                        try {
+                            JSONObject InsertUserJsonObj =  DatabaseConnectionObj.insertNewBook(
+                                    BookTitleTextView.getText().toString(),
+                                    AuthorNameTextView.getText().toString(),
+                                    BookCategory.getText().toString(),
+                                    BookFaculty.getText().toString(),
+                                    BookConditionTextView.getText().toString(),
+                                    Integer.parseInt(ISBNTextView.getText().toString()),
+                                    true,
+                                    Float.parseFloat(SellingPriceTextView.getText().toString()),
+                                    1
+                            );
+                            if ((Boolean) InsertUserJsonObj.get("Result")) {
+                                // If Result from Json is true then user has been created
+                                // and the user is redirected
+                                Snackbar.make(view, "Book Listed!", Snackbar.LENGTH_SHORT).show();
+                                //Intent redirectLoginActivity = new Intent(create_listing.this,LoginActivity.class);
+                                //startActivity(redirectLoginActivity);
+                            } else {
+                                // display the error message
+                                String MessageStr = (String) InsertUserJsonObj.get("Message");
+                                Snackbar.make(view, MessageStr, Snackbar.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Log.d("Register User Error", String.valueOf(e));
+                        }
+
+                        //___________________________
                     }
                 }
             }
