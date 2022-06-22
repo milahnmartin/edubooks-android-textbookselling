ea353e3bdc8f0dff23689e52dc779b561aa46c85
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-06-22 14:59:11 +0200

    fix(UnitTest): Removed Dummy Test, Addded Password Creation Test Method

--- app/src/test/java/edubooks/main/controllers/PasswordCreationTest.java
+++ app/src/test/java/edubooks/main/controllers/PasswordCreationTest.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 
 public class PasswordCreationTest {
     @Test
-    public void passwordHash(){
+    public void passwordHashJake(){
         assertEquals("b61a65a38fb3aa7752291cd4f1f0dbfd",PasswordCreation.encryptPassword("#@Jake1234"));
+    }
+
+    @Test
+    public void passwordHashJames(){
+        assertEquals("419e4df15edd79256deac18054000a4b",PasswordCreation.encryptPassword("#@James1234"));
     }
 }
